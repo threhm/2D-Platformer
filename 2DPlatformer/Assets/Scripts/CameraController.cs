478427c20b685643438b05e1e378845878e8c75c
threhm <threhm1@gmail.com>
2023-02-21 05:26:23 -0600

    Changed the camera to follow player Y movement

--- 2DPlatformer/Assets/Scripts/CameraController.cs
+++ 2DPlatformer/Assets/Scripts/CameraController.cs
@@ -14,9 +14,15 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Control x movement
         int playerX = (int) player.transform.position.x;
         playerX = playerX / 64;
         int camX = (playerX * 64) + 32;
         transform.position = new Vector3( camX, transform.position.y, transform.position.z);
+
+        int playerY = (int) player.transform.position.y;
+        playerY = playerY / 40;
+        int camY = (playerY * 40) + 20;
+        transform.position = new Vector3( transform.position.x, camY, transform.position.z);
     }
 }
