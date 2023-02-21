d090e0e18a2930dde281422936fa7494ec590d13
threhm <threhm1@gmail.com>
2023-02-21 08:55:58 -0600

    Created horizontal and vertical moving platforms

---
+++ 2DPlatformer/Assets/Scripts/HorizontalWallMovement.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalWallMovement : MonoBehaviour
+{
+    public float topX;
+    public float bottomX;
+
+    public float speed;
+    public bool movingLeft = false;
+    private Rigidbody2D rb;
+    private GameObject player;
+    private bool movePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        movePlayer = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Vector3 movement = new Vector3();
+        float positionX = transform.position.x;
+        if(movingLeft) {
+            if(positionX < (topX)) {
+                positionX += speed;
+            } else {
+                movingLeft = false;
+            }
+        } else if (!movingLeft) {
+            if(positionX > (bottomX)) {
+                positionX -= speed;
+            } else {
+                movingLeft = true;
+            }
+        }
+
+        if(movePlayer) {
+            if(movingLeft) {
+                player.transform.position = new Vector2(player.transform.position.x + speed, player.transform.position.y);
+            }
+            else {
+                player.transform.position = new Vector2(player.transform.position.x - speed, player.transform.position.y);
+            }
+        }
+        this.transform.position = new Vector2(positionX, transform.position.y);
+        rb.velocity = new Vector2(0, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("Hit");
+        if(other.gameObject.CompareTag("Player")) {
+            //Rigidbody2D playerRB = other.gameObject.GetComponent<Rigidbody2D>();
+            player = other.gameObject;
+            movePlayer = true;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other) {
+        //Debug.Log("Hit");
+        if(other.gameObject.CompareTag("Player")) {
+            player = other.gameObject;
+            movePlayer = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other) {
+        //Debug.Log("Hit");
+        if(other.gameObject.CompareTag("Player")) {
+            //player = other.gameObject;
+            movePlayer = false;
+        }
+    }
+}
