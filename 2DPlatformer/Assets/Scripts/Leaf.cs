6919b9f7972f3d7aed856e728fa24ae931b6a478
VioletShi-_- <passionatefrisbee@gmail.com>
2023-02-28 13:00:28 +0100

    falling leaves working

--- 2DPlatformer/Assets/Scripts/Leaf.cs
+++ 2DPlatformer/Assets/Scripts/Leaf.cs
@@ -1,61 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Leaf : MonoBehaviour
 {
     //bool falling = false;
     
+    
+    public float downForce = 0.01f;
+    bool firstTime = true;
+    SpriteRenderer sp;
     Rigidbody2D rg;
     PolygonCollider2D pc;
-    public float downForce = 10f;
-    bool firstTime = true;
-    //SpriteRenderer sp;
+
+
     float fadingTimer = 2f;
-    float fallingTimer = 5f;
+    float fallingTimer = 2f;
+    public GameObject player;
+    BoxCollider2D playerCollider;
+
+    bool fadingStarts = false;
 
     private List<GameObject> leaves = new List<GameObject>();
+    private List<SpriteRenderer> leafSprites = new List<SpriteRenderer>();
     // Start is called before the first frame update
     void Start()
     {
         rg = GetComponent<Rigidbody2D>();
+
         pc = GetComponent<PolygonCollider2D>();
-        //sp.GetComponent<SpriteRenderer>();
+        pc.isTrigger = true;
+
+        sp = GetComponent<SpriteRenderer>();
+
+        playerCollider = player.gameObject.GetComponent<BoxCollider2D>();
+        
         rg.bodyType = RigidbodyType2D.Static;
+
+        // Collecting children gameObjects/components and initializing children rigidBody
         foreach (Transform child in transform)
         {
             if (child.gameObject.CompareTag("Leaf"))
             {
                 leaves.Add(child.gameObject);
+                leafSprites.Add(child.gameObject.GetComponent<SpriteRenderer>());
+
+                child.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        foreach (GameObject leaf in leaves)
+        {
+            if (leaf.gameObject.GetComponent<PolygonCollider2D>().IsTouching(playerCollider) && firstTime)
+            {
+                firstTime = false;
+                StartCoroutine(LeafFalling());
+            }
+        }
+
+        if (fadingStarts)
+        {
+            FadingEffects(fadingTimer, sp);
+            foreach (SpriteRenderer childSp in leafSprites)
+            {
+                FadingEffects(fadingTimer, childSp);
             }
         }
     }
 
     private IEnumerator LeafFalling()
     {
-        Debug.Log("Coroutine starts");
         yield return new WaitForSeconds(fallingTimer);
-        Debug.Log("Coroutine ends");
-        rg.bodyType = RigidbodyType2D.Dynamic;
-        rg.WakeUp();
-        rg.AddForce(Random.insideUnitCircle * 5);
-        rg.gravityScale = 5;
-        rg.mass = 10;
-        //FadingEffects(fadingTimer);
-        //Destroy(gameObject, fadingTimer);
+        fadingStarts = true;
+
+        Destroy(gameObject, fadingTimer);
+        rgUpdate(rg);
+        foreach (GameObject leaf in leaves)
+        {
+            rgUpdate(leaf.gameObject.GetComponent<Rigidbody2D>());
+        }
         
     }
 
-    //private void FadingEffects(float fadingTime)
-    //{
-    //    Color color = sp.color;
-    //    color.a -= Time.deltaTime * fadingTime;
-    //    sp.color = color;
-    //}
-
-    private void OnCollisionEnter2D(Collision2D col)
+    private void rgUpdate(Rigidbody2D rigidbody)
     {
-        
+        rigidbody.bodyType = RigidbodyType2D.Dynamic;
+        rigidbody.WakeUp();
+        rigidbody.AddForce(Random.insideUnitCircle * downForce);
+        rigidbody.gravityScale = 1;
+    }
+
+    private void FadingEffects(float fadingTime, SpriteRenderer targetSpriteRenderer)
+    {
+        Color color = targetSpriteRenderer.color;
+        color.a -= Time.deltaTime * fadingTime;
+        targetSpriteRenderer.color = color;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
         if (col.gameObject.CompareTag("Player") && firstTime)
         {
             firstTime = false;
