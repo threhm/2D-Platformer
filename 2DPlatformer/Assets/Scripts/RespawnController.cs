07e3926a6674128b12e3fc976e4c8d9972d01843
threhm <threhm1@gmail.com>
2023-02-28 05:17:12 -0600

    Added respawn point turning on and off

--- 2DPlatformer/Assets/Scripts/RespawnController.cs
+++ 2DPlatformer/Assets/Scripts/RespawnController.cs
@@ -5,20 +5,50 @@ using UnityEngine;
 public class RespawnController : MonoBehaviour
 {
     private BoxCollider2D bc2D;
+    public SpriteRenderer sr;
+    public Sprite active;
+    public Sprite inactive;
+
+    public bool isActive;
+
+    private RespawnController otherRespawn;
     // Start is called before the first frame update
     void Start()
     {
         bc2D = gameObject.GetComponent<BoxCollider2D>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
         bc2D.isTrigger = true;
+        isActive = false;
         
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.CompareTag("Player")) {
             PlayerRespawn pr = other.GetComponent<PlayerRespawn>();
-            if(pr != null) {
+            //If the player has yet to touch a respawn point set this one to be the currently active one
+            if(pr.respawnPoint == null) {
                 pr.respawnPoint = this.gameObject;
+                SwitchSprite();
+            } else {
+                //Check to see if the currently active respawn point is this point, if it is do nothing
+                //otherwise set this to be the new active point
+                otherRespawn = pr.respawnPoint.gameObject.GetComponent<RespawnController>();
+                if(otherRespawn.gameObject != this.gameObject) {
+                    otherRespawn.SwitchSprite();
+                    pr.respawnPoint = this.gameObject;
+                    SwitchSprite();
+                }
             }
+        }
+    }
+
+    public void SwitchSprite() {
+        if(isActive) {
+            isActive = false;
+            sr.sprite = inactive;
+        } else {
+            isActive = true;
+            sr.sprite = active;
         }
     }
 }
