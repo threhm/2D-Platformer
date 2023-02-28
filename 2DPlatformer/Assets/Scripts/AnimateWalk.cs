e9ccf984e6a3f5c8ab4cc41ac293522a6861f991
Kerry <kjn45@case.edu>
2023-02-28 10:05:08 -0500

    Player movement updated
    New animations and movements accounted for (Walljump animation currently broken)

--- 2DPlatformer/Assets/Scripts/AnimateWalk.cs
+++ 2DPlatformer/Assets/Scripts/AnimateWalk.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public class AnimateWalk : MonoBehaviour
 {
-     private SpriteRenderer sr;
+    private SpriteRenderer sr;
     private int frameIndex;
     public Sprite[] frames;
     public float fps = 3;
     private float sliderTimer;
-
+    //need velocities to tell when to change animations
     public Sprite standingSprite;
+    public Sprite jumpSprite;
+    public Sprite wallSprite;
+    public Sprite fallSprite;
 
     void Start()
     {
@@ -19,32 +24,34 @@ public class AnimateWalk : MonoBehaviour
         frameIndex = 0;
     }
 
-    void FixedUpdate()
+    public void playerAnim(int dFacing, bool grounded, bool walled, float airtime, bool notMoving)
     {
-        if(Input.GetKey(KeyCode.LeftArrow)) {
-            sr.flipX = true;
-            sliderTimer -= Time.deltaTime;
-            if (sliderTimer <= 0)
-            {
-                frameIndex++;
-                if (frameIndex >= frames.Length) { frameIndex = 0; }
+        sr.flipX = dFacing < 0;
 
-                sliderTimer = (1f / fps);
-                sr.sprite = frames[frameIndex];
-            }
-        } else if (Input.GetKey(KeyCode.RightArrow)) {
-            sr.flipX = false;
-            sliderTimer -= Time.deltaTime;
-            if (sliderTimer <= 0)
-            {
-                frameIndex++;
-                if (frameIndex >= frames.Length) { frameIndex = 0; }
+        if (grounded) {
+            if (!notMoving) {
+                //sr.flipX = true;
+                sliderTimer -= Time.deltaTime;
+                if (sliderTimer <= 0)
+                {
+                    frameIndex++;
+                    if (frameIndex >= frames.Length) { frameIndex = 0; }
 
-                sliderTimer = (1f / fps);
-                sr.sprite = frames[frameIndex];
+                    sliderTimer = (1f / fps);
+                    sr.sprite = frames[frameIndex];
+                }
             }
-        } else {
-            sr.sprite = standingSprite;
+            else { sr.sprite = standingSprite;}
+        } else{
+            if (!walled)
+            {
+                if (airtime > 0) { sr.sprite = jumpSprite; }
+                if (airtime < 0) { sr.sprite = fallSprite; }
+            }
+            else
+            {
+                sr.sprite = wallSprite;
+            }
         }
     }
 }
--- 2DPlatformer/Assets/Scripts/PlayerController.cs
+++ 2DPlatformer/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,13 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed;
     public float horizontalAcceleration;
     public float wallJumpHeight;
-
+    private AnimateWalk spriteAnim;
     public float wallJumpPush;
 
-    
+    private int facing = 1;
+    private bool onGround;
+    private bool onWall = false;
+
     public bool doubleJump;
 
     public LayerMask groundMask;
@@ -30,14 +33,14 @@ public class PlayerController : MonoBehaviour
         
         doubleJump = true;
         reverseGrav = false;
-
+        spriteAnim = gameObject.GetComponent<AnimateWalk>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         //Check if the player is currently on the ground
-        bool onGround = false;
+        onGround = false;
         Vector2 position1 = new Vector2(transform.position.x + 0.45f, transform.position.y);
         Vector2 position2 = new Vector2(transform.position.x - 0.45f, transform.position.y);
         if(!reverseGrav) {
@@ -50,8 +53,17 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        if (rb2d.velocity.x > 0 && facing == -1)
+        {
+            facing = 1;
+        }
+        else if (rb2d.velocity.x < 0 && facing == 1)
+        {
+            facing = -1;
+        }
+
         //Handle the player moving left and right
-        if(Input.GetKey(KeyCode.LeftArrow)) {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
             if(rb2d.velocity.x > 0 && onGround) {
                 Vector2 newVelocity = new Vector2(0, rb2d.velocity.y);
                 rb2d.velocity = newVelocity;
@@ -78,12 +90,14 @@ public class PlayerController : MonoBehaviour
 
     void Update() {
         //Handle the player jumping
+        onWall= false;
         if(Input.GetKeyDown(KeyCode.Space)) {
             Vector2 position1 = new Vector2(transform.position.x + 0.4f, transform.position.y);
             Vector2 position2 = new Vector2(transform.position.x - 0.4f, transform.position.y);
             if(!reverseGrav) {
                 if (Physics2D.Raycast(position1, Vector3.down, 1.1f, groundMask) || Physics2D.Raycast(position2, Vector3.down, 1.1f, groundMask)) { 
                     rb2d.AddForce(new Vector3(0f, jumpHeight, 0f), ForceMode2D.Impulse);
+                    onWall = false;
                 } else {
                     //Jump if touching a wall. This is done in an else statement because the height from jumping off the ground should take priority
                     Vector2 position3 = new Vector2(transform.position.x, transform.position.y + 1);
@@ -91,15 +105,18 @@ public class PlayerController : MonoBehaviour
 
                     //Check if jumping off of a wall on the left
                     if (Physics2D.Raycast(position3, Vector3.left, 1.1f, groundMask) || Physics2D.Raycast(position4, Vector3.left, 1.1f, groundMask)) {
-                        rb2d.AddForce(new Vector3(wallJumpPush, wallJumpHeight, 0f), ForceMode2D.Impulse); 
+                        rb2d.AddForce(new Vector3(wallJumpPush, wallJumpHeight, 0f), ForceMode2D.Impulse);
+                        onWall = true;
                     } // Otherwise check if we are jumping off a wall on the right
                     else if (Physics2D.Raycast(position3, Vector3.right, 1.1f, groundMask) || Physics2D.Raycast(position4, Vector3.right, 1.1f, groundMask)) {
-                        rb2d.AddForce(new Vector3(-wallJumpPush, wallJumpHeight, 0f), ForceMode2D.Impulse); 
+                        rb2d.AddForce(new Vector3(-wallJumpPush, wallJumpHeight, 0f), ForceMode2D.Impulse);
+                        onWall = true;
                     }
                 }
             } else {
                 if (Physics2D.Raycast(position1, Vector3.up, 1.1f, groundMask) || Physics2D.Raycast(position2, Vector3.up, 1.1f, groundMask)) { 
                     rb2d.AddForce(new Vector3(0f, -jumpHeight, 0f), ForceMode2D.Impulse);
+                    onWall = false;
                 } else {
                     //Jump if touching a wall. This is done in an else statement because the height from jumping off the ground should take priority
                     Vector2 position3 = new Vector2(transform.position.x, transform.position.y + 1);
@@ -107,16 +124,18 @@ public class PlayerController : MonoBehaviour
 
                     //Check if jumping off of a wall on the left
                     if (Physics2D.Raycast(position3, Vector3.left, 1.1f, groundMask) || Physics2D.Raycast(position4, Vector3.left, 1.1f, groundMask)) {
-                        rb2d.AddForce(new Vector3(wallJumpPush, -wallJumpHeight, 0f), ForceMode2D.Impulse); 
+                        rb2d.AddForce(new Vector3(wallJumpPush, -wallJumpHeight, 0f), ForceMode2D.Impulse);
+                        onWall= true;
                     } // Otherwise check if we are jumping off a wall on the right
                     else if (Physics2D.Raycast(position3, Vector3.right, 1.1f, groundMask) || Physics2D.Raycast(position4, Vector3.right, 1.1f, groundMask)) {
-                        rb2d.AddForce(new Vector3(-wallJumpPush, -wallJumpHeight, 0f), ForceMode2D.Impulse); 
+                        rb2d.AddForce(new Vector3(-wallJumpPush, -wallJumpHeight, 0f), ForceMode2D.Impulse);
+                        onWall= true;
                     }
                 }
             }
         }
 
-
+        spriteAnim.playerAnim(facing, onGround, onWall, rb2d.velocity.y, rb2d.velocity.x == 0);
 
     }
 
